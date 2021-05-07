1abb726decb79e7c2030905c65e9b46ac1caa12c
kmalakhova <kgrekhova@gmail.com>
2021-05-07 16:22:21 -0700

    Minor changes that don't affect functionality

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,28 @@
 from app import db
 from app.models.task import Task
 from app.models.goal import Goal
-from flask import request, Blueprint, jsonify # make_response,
+from flask import request, Blueprint, jsonify
 from sqlalchemy import asc, desc
 from datetime import datetime
 import os
 # from slack_sdk import WebClient
-import requests
+from requests import post
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
+####################### TASK ROUTES #######################
+
 @tasks_bp.route("", methods = ["POST"])
 def add_task():
     """Adds new task to the Task table"""
-    response_body = request.get_json()
-    if len(response_body) != 3:
+    request_body = request.get_json()
+    if len(request_body) != 3:
         return jsonify({"details": f"Invalid data"}), 400
     task = Task(
-        title = response_body["title"],
-        description = response_body["description"],
-        completed_at = response_body["completed_at"]
+        title = request_body["title"],
+        description = request_body["description"],
+        completed_at = request_body["completed_at"]
         )  
     db.session.add(task)
     db.session.commit()
@@ -84,7 +86,6 @@ def mark_task_complete(task_id):
         return jsonify(None), 404
     if not task.is_complete(): 
         task.completed_at = datetime.now()
-        db.session.add(task)
         db.session.commit()
         send_slack_task_notification(task)
     return jsonify({"task": task.to_dict()}), 200
@@ -104,7 +105,7 @@ def send_slack_task_notification(task):
     notification = f"Someone just completed the task {task.title}"
     url = f"https://slack.com/api/chat.postMessage?channel=task-notifications&text={notification}"
     headers = {"Authorization": f"Bearer {SLACK_BOT_TOKEN}"}
-    return requests.request("POST", url, headers=headers)
+    return post(url, headers=headers)
 
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def mark_task_incomplete(task_id):
@@ -114,17 +115,18 @@ def mark_task_incomplete(task_id):
         return jsonify(None), 404
     if task.is_complete(): 
         task.completed_at = None
-        db.session.add(task)
         db.session.commit()
     return jsonify({"task": task.to_dict()}), 200
+
+####################### GOAL ROUTES #######################
 
 @goals_bp.route("", methods = ["POST"])
 def add_goal():
     """Adds a new goal to the Goal table"""
-    response_body = request.get_json()
-    if "title" not in response_body:
+    request_body = request.get_json() 
+    if "title" not in request_body:
         return jsonify({"details": "Invalid data"}), 400
-    goal = Goal(title = response_body["title"])  
+    goal = Goal(title = request_body["title"])  
     db.session.add(goal)
     db.session.commit()
     return jsonify({"goal": goal.to_dict()}), 201
