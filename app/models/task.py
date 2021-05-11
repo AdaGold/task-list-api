e9e2a1ad28499b66208aaf640f9da4f4ec5c1676
kmalakhova <kgrekhova@gmail.com>
2021-05-11 09:27:47 -0700

    Add one-to-many relationship between goals and tasks

--- app/models/task.py
+++ app/models/task.py
@@ -7,6 +7,7 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"), nullable=True)
 
     def is_complete(self):
         if self.completed_at is None:
