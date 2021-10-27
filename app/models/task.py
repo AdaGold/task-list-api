02ed8d5d0398dfd96ccc3254b778a53371bb7009
Reid Maute <reidhowdy@gmail.com>
2021-10-27 14:27:29 -0700

    set up and model created and migrated

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,14 @@
+from operator import truediv
 from flask import current_app
 from app import db
 
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    is_complete = db.Column(db.Boolean, default=False)
+
+
+
