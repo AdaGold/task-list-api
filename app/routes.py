127f86e110b502cc471326a0ae7063daafef35de
Asli Athman <asliathman@gmail.com>
2021-10-28 17:17:21 -0400

    added /task/<id> route, passed third test of wave 1

--- app/routes.py
+++ app/routes.py
@@ -10,19 +10,37 @@ def handle_tasks():
         tasks = Task.query.all()
         tasks_response = []
         for task in tasks:
-            if not task.completed_at:
-                is_complete = False
-            else:
-                is_complete = True
+            # if not task.completed_at:
+            #     is_complete = False
+            # else:
+            #     is_complete = True
+                
             tasks_response.append({
                 "id" : task.id,
                 "title" : task.title,
                 "description" : task.description,
-                "is_complete" : is_complete
+                "is_complete" : bool(task.completed_at)
             })
 
         return jsonify(tasks_response)
 
+@tasks_bp.route("/<id>", methods=["GET", "PUT", "DELETE"])
+def handle_task(id):
+    task = Task.query.get(id)
+    if task is None:
+        return jsonify(""), 201
+    
+    if request.method == "GET":
+        return {
+            "task": {
+                "id": task.id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": bool(task.completed_at)
+            }
+    }
+
+    
     # elif request.method == "POST":
     #     request_body = request.get_json()
     #     new_task = Task(title=request_body["title"], 
@@ -30,16 +48,37 @@ def handle_tasks():
 
     #     db.session.add(new_task)
     #     db.session.commit()
-
-    #     return jsonify()
-#GET /tasks
-    #one saved task
+        
+    #     if request_body["completed_at"]:
+    #         is_complete = True
+    #     else: 
+    #         is_complete = False
+        
+    #     return jsonify("succesfully added"), 201
 
 #POST /tasks
     #create a task where completed @ == None
     #ERROR: task with missing 'title' field
     #ERROR: task with missing 'description' field
     #create a task must contain completed @ (?????)
+
+# @tasks_bp.route("/<id>", methods=["GET", "PUT", "DELETE"])
+# def handle_tasks(id):
+#     task = Task.query.get(id)
+#     if task is None:
+#         return jsonify(""), 201
+    
+#     if request.method == "GET":
+#         if not task.completed_at:
+#                 is_complete = False
+#         else:
+#                 is_complete = True
+#         return {
+#                 "id" : task.id,
+#                 "title" : task.title,
+#                 "description" : task.description,
+#                 "is_complete" : is_complete
+#         }
 
 #GET /tasks/1
     #get existing taks
