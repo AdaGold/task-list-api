b960939277661c7637a782b1d252d6c4785b4a40
ayaka <ayakaob@gmail.com>
2021-10-29 15:46:49 -0700

    route POST /tasks

--- app/models/task.py
+++ app/models/task.py
@@ -5,8 +5,12 @@ from app import db
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30), nullable = False)
-    description = db.Column(db.String(300))
+    description = db.Column(db.String(300), nullable = False)
     completed_at = db.Column(db.DateTime)
+
+    @classmethod
+    def from_dict(cls, task_dict):
+        return cls(**task_dict)
 
     def to_dict(self):
         is_complete = False if self.completed_at is None else True
--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,26 @@ from flask import Blueprint, request, jsonify
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-@tasks_bp.route("", methods=["GET"])
+@tasks_bp.route("", methods=["GET", "POST"])
 def tasks():
-    tasks = Task.query.all()
-    response = [task.to_dict() for task in tasks]
-    return jsonify(response), 200
+    if request.method == "GET":
+        tasks = Task.query.all()
+        response = [task.to_dict() for task in tasks]
+        return jsonify(response), 200
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+
+        if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
+            return {"details": "Invalid data"}, 400
+
+        task = Task.from_dict(request_body)
+
+        db.session.add(task)
+        db.session.commit()
+
+        response_body={
+            "task": task.to_dict()
+        }
+
+        return jsonify(response_body), 201
