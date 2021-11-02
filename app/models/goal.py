0af41c3db285904895976ea50a82e9f846861b28
Asli Athman <asliathman@gmail.com>
2021-11-02 16:54:05 -0400

    updated goal model to make title column not nullable, added a new migration

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,4 +4,4 @@ from app import db
 
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String, nullable=False)
