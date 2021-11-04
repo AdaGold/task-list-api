c97245b1b6d0d942224ff929bf3da9dce965f511
Shaina Beth <shainabeth94@gmail.com>
2021-11-03 20:23:07 -0700

    completed wave 6

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,6 +5,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
+    tasks = db.relationship("Task", back_populates="goal", lazy=True)
 
     def goal_dict(self):
         return {
--- app/routes.py
+++ app/routes.py
@@ -9,20 +9,21 @@ import os
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
-    
+
     if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
         return jsonify({"details": "Invalid data"}), 400
     
     new_task = Task(title=request_body["title"], 
-    description=request_body["description"],
-    completed_at=request_body["completed_at"])
+        description=request_body["description"],
+        completed_at=request_body["completed_at"])
 
     db.session.add(new_task)
     db.session.commit()
-
+    
     return jsonify({"task": new_task.task_dict()}), 201
 
 @tasks_bp.route("", methods=["GET"])
@@ -176,3 +177,41 @@ def delete_goal(goal_id):
     
     return jsonify({"details": f"Goal {goal_id} \"{goal.title}\" successfully deleted"}), 200
 
+@goals_bp.route("/<goal_id>/tasks", methods=["POST"])
+def add_task_to_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+    if goal is None:
+        return "", 404
+
+    request_body = request.get_json()
+    if "task_ids" not in request_body:
+        return jsonify({"details": "Invalid data"}), 400
+
+    for task_id in request_body["task_ids"]:
+        task = Task.query.get(task_id)
+        task.goal = goal
+        goal.tasks.append(task)
+        
+    db.session.commit()
+
+    return jsonify({
+        "id": goal.goal_id,
+        "task_ids": request_body["task_ids"]
+    }), 200
+
+
+@goals_bp.route("/<goal_id>/tasks", methods=["GET"])
+def get_tasks_for_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+    if goal is None:
+        return "", 404
+
+    tasks = []
+    for task in goal.tasks:
+        tasks.append(task.task_dict())
+
+    return jsonify({
+        "id": goal.goal_id,
+        "title": goal.title,
+        "tasks": tasks
+    }), 200
