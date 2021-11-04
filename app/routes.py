bc4f58a7465fe20cd70cc6c9f4f3bd9a9e7cf7fd
Asli Athman <asliathman@gmail.com>
2021-11-04 12:09:22 -0400

    refactored /tasks POST method to check for if 'completed_at' was given in the response body

--- app/routes.py
+++ app/routes.py
@@ -31,9 +31,12 @@ def read_tasks():
 def create_task():
     request_body = request.get_json()
     try:
-        new_task = Task(title=request_body["title"],
-                        description=request_body["description"], completed_at=request_body["completed_at"])
-
+        if request_body["completed_at"]:
+            new_task = Task(title=request_body["title"],
+                            description=request_body["description"], completed_at=request_body["completed_at"])
+        else:
+            new_task = Task(title=request_body["title"], description=request_body["description"], completed_at=None)
+            
         db.session.add(new_task)
         db.session.commit()
 
