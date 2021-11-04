c2185201f0d8574bac17693213e56a54984f6a8f
Asli Athman <asliathman@gmail.com>
2021-11-04 11:43:25 -0400

    FINISHED ALL WAVES, PASSED ALL TESTS, final draft to deploy

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,6 +5,9 @@ from app import db
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
-
     tasks = db.relationship("Task", backref="goal", lazy=True)
+    
+    def to_dict(self, task=None):
+        pass
+
     
--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,4 @@
-from flask import current_app, jsonify
+from flask import current_app
 from app import db
 
 
