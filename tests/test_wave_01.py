91f1bb743901938890f15cb7082808aac727dc58
soridalac <lacsorida@gmail.com>
2022-05-07 14:38:27 -0700

    adding the assert to wave 01

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -59,13 +59,8 @@ def test_get_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-    
-    
-
-    #raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
+    assert response_body == {"message": "Task 1 not found"}
+    assert len(response_body) == 1
 
 
 #@pytest.mark.skip(reason="No way to test this feature yet")
@@ -132,14 +127,9 @@ def test_update_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-    assert response_body == None
-
-    #raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
-
-
+    assert response_body == {"message": "Task 1 not found"}
+    assert len(response_body) == 1
+    
 #@pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task(client, one_task):
     # Act
@@ -163,15 +153,9 @@ def test_delete_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-
-    #raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
-
+    assert response_body == {"message": 'Task 1 not found'}
+    assert "message" in response_body
     assert Task.query.all() == []
-
 
 #@pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_task_must_contain_title(client):
