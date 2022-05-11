2b2c23b312b11298e4ec2ace0ff7407bd3ebae16
soridalac <lacsorida@gmail.com>
2022-05-11 10:02:17 -0700

    edit the goal and task models

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,18 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
+
+
+    def to_dict(self):
+        task_dict = {
+        "id": self.task_id,
+        "title": self.title, 
+        "description": self.description, 
+        "is_complete": bool(self.completed_at)}
+
+        if self.goal_id:
+            task_dict["goal_id"] = self.goal_id
+
+        return task_dict
+
