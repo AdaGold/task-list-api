e864b6027830d66005dcd3ae41d4d8843fcd137a
soridalac <lacsorida@gmail.com>
2022-05-12 10:33:14 -0700

    refactor goal and task route

--- app/routes/goal_route.py
+++ app/routes/goal_route.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, make_response, jsonify, abort
+from flask import Blueprint, request, jsonify
 from app import db
 from app.models.goal import Goal
+from app.routes_helper import validated_goal, validated_task
 from app.models.task import Task
 
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
@@ -18,62 +19,25 @@ def create_goal():
     db.session.add(new_goal)
     db.session.commit()
 
-    return jsonify({"goal": new_goal.to_dict()}), 201
-
-def validated_goal(goal_id):
-    try:
-        goal_id = int(goal_id)
-    except ValueError:
-        rsp = {"msg": f"Invalid id: {goal_id}"}
-        abort(make_response(jsonify(rsp), 400))
-
-    goal = Goal.query.get(goal_id)
-
-    if goal is None:
-        rsp = {"msg":f"Goal {goal_id} not found"}
-        abort(make_response(jsonify(rsp), 404))
-
-    return goal
-
-def validated_task(task_id):
-    try:
-        task_id = int(task_id)
-    except ValueError:
-        rsp = {"msg": f"Invalid id: {task_id}"}
-        abort(make_response(jsonify(rsp), 400))
-
-    task = Task.query.get(task_id)
-
-    if task is None:
-        rsp = {"message":f"Task {task_id} not found"}
-        abort(make_response(jsonify(rsp), 404))
-
-    return task
+    return {"goal": new_goal.to_dict()}, 201
 
 @goals_bp.route("/<goal_id>", methods=["GET"])
 def get_one_goal(goal_id):
-    goal = validated_goal(goal_id)
+    goal = validated_goal(Goal, goal_id)
 
-    return jsonify(
-        {"goal": goal.to_dict()}), 200
+    return {"goal": goal.to_dict()}, 200
 
 @goals_bp.route("", methods=["GET"])
 def get_all_goals():
     goals = Goal.query.all()
 
-    goals_response = []
-    for goal in goals:
-        goals_response.append({
-            "id": goal.goal_id,
-            "title": goal.title,
-        })
+    goals_response = [goal.to_dict() for goal in goals]
 
     return jsonify(goals_response)
 
 @goals_bp.route("/<goal_id>", methods=["PUT"])
 def update_goal(goal_id):
-    # goal = Goal.query.get(goal_id)
-    goal = validated_goal(goal_id)
+    goal = validated_goal(Goal, goal_id)
     request_body = request.get_json()
     goal.title = request_body["title"]
 
@@ -83,7 +47,7 @@ def update_goal(goal_id):
 
 @goals_bp.route("/<goal_id>", methods=["DELETE"])
 def delete_one_goal(goal_id):
-    goal = validated_goal(goal_id)
+    goal = validated_goal(Goal, goal_id)
 
     db.session.delete(goal)
     db.session.commit()
@@ -92,14 +56,13 @@ def delete_one_goal(goal_id):
 
 @goals_bp.route("/<goal_id>/tasks", methods=["POST"])
 def goal_to_task(goal_id):
-    goal = validated_goal(goal_id)
+    goal = validated_goal(Goal, goal_id)
     request_body = request.get_json()
 
     for task_id in request_body["task_ids"]:
-        task = validated_task(task_id)
+        task = validated_task(Task, task_id)
     
         goal.tasks.append(task)
-
     # db.session.add(task)
     db.session.commit()
 
@@ -110,7 +73,7 @@ def goal_to_task(goal_id):
 
 @goals_bp.route("/<goal_id>/tasks", methods=["GET"])
 def get_goal_task(goal_id):
-    goal = validated_goal(goal_id)
+    goal = validated_goal(Goal, goal_id)
     result = {
         "id": goal.goal_id,
         "title": goal.title,
--- app/routes/task_route.py
+++ app/routes/task_route.py
@@ -1,19 +1,18 @@
 
-from flask import Blueprint, request, make_response, jsonify, abort
+from flask import Blueprint, request, make_response, jsonify
 from sqlalchemy import asc, desc
 from app import db
 from app.models.task import Task
 from sqlalchemy.sql.functions import now
 import requests, os
+from app.routes_helper import validated_task
+
 
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
 @tasks_bp.route("", methods=["POST"])
 def create_task():
-    #the client make some request
-    #request body is a dictionary that pull a request from json
-    #convert the json object into python data
     request_body = request.get_json()
     if "title" not in request_body or "description" not in request_body:
         return make_response({'details': "Invalid data"}, 400)
@@ -22,6 +21,7 @@ def create_task():
         title = request_body["title"],
         description = request_body["description"]
     )
+    
     if "completed_at" in request_body:
         new_task.completed_at = request_body["completed_at"]
 
@@ -44,53 +44,28 @@ def get_all_tasks():
         # To get all the task from the table
          tasks = Task.query.all()
 
-    tasks_response = []
-    for task in tasks:
-        tasks_response.append({
-            "id": task.task_id,
-            "description": task.description,
-            "title": task.title,
-            "is_complete": False
-        })
+    tasks_response = [task.to_dict() for task in tasks]
 
     return jsonify(tasks_response)
 
-def validated_task(task_id):
-    try:
-        task_id = int(task_id)
-    except ValueError:
-        rsp = {"msg": f"Invalid id: {task_id}"}
-        abort(make_response(jsonify(rsp), 400))
-
-    task = Task.query.get(task_id)
-
-    if task is None:
-        rsp = {"message":f"Task {task_id} not found"}
-        abort(make_response(jsonify(rsp), 404))
-
-    return task
-
-
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
-    task = validated_task(task_id)
+    task = validated_task(Task, task_id)
 
     return {"task": task.to_dict()}
 
-
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_one_task(task_id):
-    task = validated_task(task_id)
+    task = validated_task(Task, task_id)
 
     db.session.delete(task)
     db.session.commit()
 
-    return jsonify({"details": f'Task {task_id} \"{task.title}\" successfully deleted'})
-
+    return {"details": f'Task {task_id} \"{task.title}\" successfully deleted'}, 200
 
 @tasks_bp.route('/<task_id>/mark_complete', methods=['PATCH'])
 def mark_complete_task(task_id):
-    task = validated_task(task_id)
+    task = validated_task(Task, task_id)
     task.completed_at = now()
 
     db.session.add(task)
@@ -108,12 +83,11 @@ def mark_complete_task(task_id):
 
     url = requests.post(SLACK_API_URL, headers=headers, params=query_params)
 
-
     return {"task": task.to_dict()}, 200
 
 @tasks_bp.route('/<task_id>/mark_incomplete', methods=["PATCH"])
 def mark_incomplete_task(task_id):
-    task = validated_task(task_id)
+    task = validated_task(Task, task_id)
     task.completed_at = None
 
     db.session.add(task)
@@ -123,11 +97,14 @@ def mark_incomplete_task(task_id):
 
 @tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_task_complete_date(task_id):
-    task = validated_task(task_id)
+    task = validated_task(Task, task_id)
     request_body = request.get_json()
     
     task.title = request_body["title"]
     task.description = request_body["description"]
+
+    # new_task = Task.from_dict(request_body)
+    # new_task.task = task
 
     db.session.add(task)
     db.session.commit()
