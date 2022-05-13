45e534a1f4c78e437b5947ce0ad424192ef5907c
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-05-13 12:15:16 -0400

    fixing /goals/id/tasks/create endpoing

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -168,7 +168,8 @@ def create_task_with_goal(id):
     
     request_body = request.get_json()
     new_task = Task.from_dict(request_body)
-    new_task.goal = goal
+    new_task.goal_id = goal.id
+    new_task.goal = goal.title
 
     db.session.add(new_task)
     db.session.commit()
