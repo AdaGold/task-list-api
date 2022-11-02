60f373e8481c34100b2dc4e77b96c03e832ac52b
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-11-02 14:05:26 -0700

    Created route POST route and create_one_task function

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models.task import Task
 from app import db
 
 task_bp = Blueprint("task", __name__, url_prefix="/task")
 
+@task_bp.route("", methods=["POST"])
+def create_one_task():
+    request_body = request.get_json()
 
+    new_task = Task(
+        title=request_body["title"],
+        description=request_body["description"],
+        completed_at=request_body["completed_at"]
+    )
+    db.session.add(new_task)
+    db.session.commit()
