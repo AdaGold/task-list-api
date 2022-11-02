07fa7774dc03593c1b2768a9125dd0dddac29d5a
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-11-02 14:00:39 -0700

    In routes.py added imports and created Blueprint

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models.task import Task
+from app import db
+
+task_bp = Blueprint("task", __name__, url_prefix="/task")
+
+
