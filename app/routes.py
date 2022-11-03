283eab63e9706c037391e3752afe19c76f9529ed
Aisha Husseini <ayasha.husseini@gmail.com>
2022-11-03 18:23:09 -0400

    Passing tests

--- app/routes.py
+++ app/routes.py
@@ -73,6 +73,8 @@ def update_task(task_id):
     request_body = request.get_json()
     task.title = request_body["title"]
     task.description = request_body["description"]
+    task.completed_at =request_body["completed_at"]
+
     
     db.session.commit()
 
