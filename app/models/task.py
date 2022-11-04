afa7ed923a22d8b85cc03b6da542298a59024988
Neida <neida@duck.com>
2022-11-03 21:31:56 -0400

    creates PUT request to create new task

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,38 @@
 from app import db
+from flask import make_response, abort
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    def to_json(self):
+        if self.completed_at:
+            is_complete = True
+        else:
+            is_complete = False
+
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": is_complete
+        }
+
+    @classmethod
+    def from_json(cls, request_body):
+        return cls(
+            title=request_body["title"],
+            description=request_body["description"],
+            # completed_at=request_body["completed_at"] # Why do I havce to comment this out? 
+        )
+    
+    def update(self,request_body):
+        try:
+            self.title = request_body["title"]
+            self.completed_at = request_body["completed_at"]
+            self.description = request_body["description"]
+        except KeyError as error:
+            abort(make_response({'message': f"Missing attribute: {error}"}))
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.task import Task
+import os, requests
+
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# ================================
+# Create One Task 
+# ================================
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_json(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify({"task": new_task.to_json()}), 201
