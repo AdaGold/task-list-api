23322371c6bd0b7e09443ea8ac0f72189e7ddf67
Nicole Mejia <snicolemejia@gmail.com>
2022-11-06 10:53:55 -0500

    update post method to handle KeyError

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,29 +1,37 @@
 from app import db
 from app.models.task import Task
-from flask import Blueprint, request, make_response, jsonify
+from flask import Blueprint, request, make_response, jsonify, abort
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
 # CREATE ONE TASK w/ POST REQUEST
 @tasks_bp.route("", methods=['POST'])
 def create_task():
-    # get post request data and convert to json
-    request_body = request.get_json()
+    # use try / except to catch KeyError for invalid data
+    try:
+        # get post request data and convert to json
+        request_body = request.get_json()
 
-    # make a new instance of Task using request data
-    new_task = Task(
-        title=request_body["title"],
-        description=request_body["description"]
-        )
+        # make a new instance of Task using request data
+        new_task = Task(
+            title=request_body["title"],
+            description=request_body["description"]
+            )
 
-    # add new Task to database 
-    db.session.add(new_task)
-    db.session.commit()
+        # add new Task to database 
+        db.session.add(new_task)
+        db.session.commit()
 
-    # return new task in json and successfully created status code
-    return make_response(jsonify({"task": 
+        # return new task in json and successfully created status code
+        return make_response(jsonify({"task": 
                     {"id": new_task.task_id,
                     "title": new_task.title,
                     "description": new_task.description,
                     "is_complete": new_task.is_complete
                     }}), 201)
+    
+    except KeyError:
+        # abort and show error message if KeyError
+        abort(make_response({"details": "Invalid data"}, 400))
+
+    
