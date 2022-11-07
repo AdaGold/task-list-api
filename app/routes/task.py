a245770fcb543b020582a37e1d55fe584e2c90be
dainiz <dainiz.almazan@gmail.com>
2022-11-07 01:09:23 -0800

    Creates endpoints for reading one task and reading all tasks

--- app/routes/task.py
+++ app/routes/task.py
@@ -1,9 +1,22 @@
 from app import db
 from app.models.task import Task
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort
 
 tasks_bp = Blueprint("task", __name__, url_prefix="/tasks")
 
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message": f"{cls.__name__} {model_id} is not a valid id"}, 400))
+
+    model = cls.query.get(model_id)
+    if not model:
+        abort(make_response({"message": f"{cls.__name__} {model_id} not found"}, 404))
+    
+    return {cls.__name__: model.to_dict()}
+
+# CREATE
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
@@ -14,3 +27,20 @@ def create_task():
 
     created_task = {"task": new_task.to_dict()}
     return make_response(created_task, 201)
+
+# READ
+@tasks_bp.route("", methods=["GET"])
+def handle_tasks():
+    tasks = Task.query.all()
+    tasks_response = [task.to_dict() for task in tasks]
+
+    return jsonify(tasks_response), 200
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def handle_one_task(task_id):
+    task = validate_model(Task, task_id)
+    return jsonify(task), 200
+
+# UPDATE
+
+# DELETE
