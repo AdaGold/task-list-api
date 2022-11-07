4adaea1be6e65479d5b9364e4d6ff0268d8160e1
Kindra <kindra.kathleen@gmail.com>
2022-11-07 11:38:21 -0500

    refactored POST and GET routes to be seperate

--- app/routes.py
+++ app/routes.py
@@ -4,23 +4,50 @@ from flask import Blueprint, jsonify, make_response, request
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
-@tasks_bp.route("", methods=["GET","POST"])
-def handle_tasks():
-    if request.method == "GET":
-        tasks = Task.query.all()
-        tasks_response = []
-        for task in tasks:
-            tasks_response.append({
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"])
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"Task {new_task.title} successfully created", 201 )
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks_response = []
+    tasks = Task.query.all()
+    for task in tasks:
+        tasks_response.append(
+            {
                 "id": task.task_id,
                 "title": task.title,
                 "description": task.description,
                 "completed at": task.completed_at
-            })
-        return jsonify(tasks_response)
-    elif request.method == "POST":
-        request_body = request.get_json()
-        new_task = Task(title=request_body["title"],
-                        description=request_body["description"])
-        db.session.add(new_task)
-        db.session.commit()
-        return make_response(f"Task {new_task.title} successfully created", 201)
+            }
+        )
+    return jsonify(tasks_response)
+    
+    
+    
+    
+    
+    # if request.method == "GET":
+    #     tasks = Task.query.all()
+    #     tasks_response = []
+    #     for task in tasks:
+    #         tasks_response.append({
+    #             "id": task.task_id,
+    #             "title": task.title,
+    #             "description": task.description,
+    #             "completed at": task.completed_at
+    #         })
+    #     return jsonify(tasks_response)
+    # elif request.method == "POST":
+    #     request_body = request.get_json()
+    #     new_task = Task(title=request_body["title"],
+    #                     description=request_body["description"])
+    #     db.session.add(new_task)
+    #     db.session.commit()
+    #     return make_response(f"Task {new_task.title} successfully created", 201)
