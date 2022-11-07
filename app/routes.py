23c112f837a9511434cfe8324fa342e314e660ed
Erika Sha <erikaasha05@gmail.com>
2022-11-06 22:22:39 -0500

    created routes for GET /tasks and /tasks/<task_id> and validate_id helper function

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 from app import db
 from app.models.task import Task
-from flask import Blueprint, request, make_response, jsonify
+from flask import Blueprint, request, make_response, jsonify, abort
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+def validate_id(cls, model_id):
+    try: 
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+    
+    model = cls.query.get(model_id)
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
 
 @tasks_bp.route("", methods=["POST"])
 def create_task():
@@ -14,3 +26,15 @@ def create_task():
 
     response = {"task": new_task.to_dict()}
     return make_response(jsonify(response), 201)
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = [task.to_dict() for task in tasks]
+
+    return jsonify(tasks_response)
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = validate_id(Task, task_id)
+    return jsonify(task.to_dict())
