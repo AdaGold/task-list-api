e43c07bd4ca3c95573a080bb94c97f1934f3a7b3
Erika Sha <erikaasha05@gmail.com>
2022-11-06 22:08:08 -0500

    created route for POST /tasks

--- app/models/task.py
+++ app/models/task.py
@@ -1,8 +1,30 @@
 from app import db
 
-
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
+
+    def to_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.task_id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        if self.completed_at == None:
+            task_as_dict["is_complete"] = False
+        else:
+            task_as_dict["is_complete"] = task_as_dict["completed_at"]
+        
+        return task_as_dict
+
+    @classmethod
+    def from_dict(cls, task_data):
+        if not task_data.get("completed_at", None):
+            new_task = Task(title=task_data["title"],
+                        description=task_data["description"])
+        else:
+            new_task = Task(title=task_data["title"],
+                            description=task_data["description"],
+                            completed_at=task_data["completed_at"])
+        return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,16 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, request, make_response, jsonify
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = {"task": new_task.to_dict()}
+    return make_response(jsonify(response), 201)
