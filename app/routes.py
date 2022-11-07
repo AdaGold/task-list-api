74c05e873e2923c05e45cf3d858b3440f265c3b8
Kindra <kindra.kathleen@gmail.com>
2022-11-07 12:16:41 -0500

    created read one task route

--- app/routes.py
+++ app/routes.py
@@ -29,25 +29,17 @@ def read_all_tasks():
         )
     return jsonify(tasks_response)
     
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = Task.query.get(task_id)
+
+    return {
+        "id": task.task_id,
+        "title": task.title,
+        "description": task.description,
+        "completed at": task.completed_at
+    }
     
     
     
     
-    # if request.method == "GET":
-    #     tasks = Task.query.all()
-    #     tasks_response = []
-    #     for task in tasks:
-    #         tasks_response.append({
-    #             "id": task.task_id,
-    #             "title": task.title,
-    #             "description": task.description,
-    #             "completed at": task.completed_at
-    #         })
-    #     return jsonify(tasks_response)
-    # elif request.method == "POST":
-    #     request_body = request.get_json()
-    #     new_task = Task(title=request_body["title"],
-    #                     description=request_body["description"])
-    #     db.session.add(new_task)
-    #     db.session.commit()
-    #     return make_response(f"Task {new_task.title} successfully created", 201)
