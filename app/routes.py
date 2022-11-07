0372ca559391d2fad5a156ea159ea85b9382868d
Kindra <kindra.kathleen@gmail.com>
2022-11-07 11:08:40 -0500

    initial set up & create new task route created

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,16 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def handle_tasks():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"])
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"Task {new_task.title} successfully created", 201)
+    
