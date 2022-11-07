34f9f619ea07051dee6ba76d1725aac9da28d49a
Anaiq <>
2022-11-07 14:28:52 -0600

    created Task model adn to_dict helper, created validate_model in routes.py, db initilized and first migrations applied.

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,24 @@
 from app import db
+import datetime
 
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True, default=datetime.datetime.utcnow)
+
+def to_dict(self):
+    task_as_dict = {}
+    task_as_dict["id"] = self.id
+    task_as_dict["title"] = self.title
+    task_as_dict["description"] = self.description
+    task_as_dict["completed_at"]= self.completed_at 
+
+def from_dict(cls, task_data):
+    new_task = Task(
+        title=task_data["title"],
+        description=task_data["description"],
+        completed_at=task_data["is_complete"] # or completed_at???#
+    )
+    return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} . not found"}, 404))
+
+
