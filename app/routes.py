4f804d3542c321aa6e4015546350060717d62c1a
Jennifer <datateur@gmail.com>
2022-11-08 09:48:00 -0800

    Created queries to sort by title asc and desc

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response, abort
 from app.models.task import Task
 from app import db
+from sqlalchemy import asc, desc
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
@@ -8,24 +9,39 @@ def validate_task(task_id):
     try:
         task_id = int(task_id)
     except:
-        abort(make_response({"message":f"Task #{task_id} invalid"}, 400))
+        abort(make_response({"message":f"Task {task_id} invalid"}, 400))
 
     task = Task.query.get(task_id)
 
     if not task:
-        abort(make_response({"message":f"Task #{task_id} not found"}, 404))
+        abort(make_response({"message":f"Task {task_id} not found"}, 404))
 
     return task
 
 @tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
-    tasks = Task.query.all()
+    sort_query = request.args.get("sort")
+    tasks_query = Task.query
+
+    if sort_query == "asc":
+        tasks_query = Task.query.order_by(asc("title"))
+    if sort_query == "desc":
+        tasks_query = Task.query.order_by(desc("title"))
+
+    tasks = tasks_query.all()
+
     tasks_response = [task.to_dict() for task in tasks]
     
     return jsonify(tasks_response), 200
 
+@tasks_bp.route("/<id>", methods=["GET"])
+def get_one_task(id):
+    task = validate_task(id)
+
+    return jsonify({"task":task.to_dict()}), 200
+
 @tasks_bp.route("", methods=["POST"])
-def create_one_task():
+def create_task():
     request_body = request.get_json()
     if ("title" not in request_body or "description" not in request_body):
         abort(make_response({"details": "Invalid data"}, 400))
@@ -39,10 +55,25 @@ def create_one_task():
 
     return jsonify({"task":new_task.to_dict()}), 201
 
-@tasks_bp.route("/<id>", methods=["GET"])
-def get_one_task(id):
+@tasks_bp.route("/<id>", methods=["PUT"])
+def update_task(id):
     task = validate_task(id)
+    request_body=request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
 
     return jsonify({"task":task.to_dict()}), 200
 
-#@tasks_bp.route("/<id>", methods=["DELETE"])
+@tasks_bp.route("/<id>", methods=["DELETE"])
+def delete_task(id):
+    task = validate_task(id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return jsonify({"details": f'Task {id} "{task.title}" successfully deleted'}), 200
+
+
--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -59,7 +59,7 @@ def test_get_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-    assert response_body == {"message":"Task #1 not found"}
+    assert response_body == {"message":"Task 1 not found"}
 
 
 #@pytest.mark.skip(reason="No way to test this feature yet")
@@ -89,7 +89,7 @@ def test_create_task(client):
     assert new_task.completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+#@pytest.mark.skip(reason="No way to test this feature yet")
 def test_update_task(client, one_task):
     # Act
     response = client.put("/tasks/1", json={
@@ -115,7 +115,7 @@ def test_update_task(client, one_task):
     assert task.completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+#@pytest.mark.skip(reason="No way to test this feature yet")
 def test_update_task_not_found(client):
     # Act
     response = client.put("/tasks/1", json={
@@ -126,11 +126,7 @@ def test_update_task_not_found(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
+    assert response_body == {"message":"Task 1 not found"}
 
 
 #@pytest.mark.skip(reason="No way to test this feature yet")
@@ -157,11 +153,11 @@ def test_delete_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    #raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
-
+    assert response_body == {"message":f"Task 1 not found"}
     assert Task.query.all() == []
 
 
