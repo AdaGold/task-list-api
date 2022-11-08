b5f570f5122678749f19d00e8523e68ed9aa1869
Anaiq <>
2022-11-07 20:32:04 -0600

    registered routes in __init__.py, unskip tests in test_wave_01, create_task in routes.py, bool to attibute in task.py

--- app/models/task.py
+++ app/models/task.py
@@ -6,19 +6,23 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime, nullable=True, default=datetime.datetime.utcnow)
+    completed_at = db.Column(db.DateTime, nullable=True)
 
-def to_dict(self):
-    task_as_dict = {}
-    task_as_dict["id"] = self.id
-    task_as_dict["title"] = self.title
-    task_as_dict["description"] = self.description
-    task_as_dict["completed_at"]= self.completed_at 
+    def to_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["is_complete"]= bool(self.completed_at)
 
-def from_dict(cls, task_data):
-    new_task = Task(
-        title=task_data["title"],
-        description=task_data["description"],
-        completed_at=task_data["is_complete"] # or completed_at???#
-    )
-    return new_task
+        return task_as_dict
+
+    @classmethod
+    def from_dict(cls, task_data):
+        return Task(
+            title=task_data["title"],
+            description=task_data["description"],
+            completed_at=None
+        )
+
+    #create helper function for is_completed?
--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,19 @@ def validate_model(cls, model_id):
     model = cls.query.get(model_id)
 
     if not model:
-        abort(make_response({"message":f"{cls.__name__} {model_id} . not found"}, 404))
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
 
 
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    
+    if not all(["title" in request_body, "description" in request_body, "completed_at" in request_body ])  :
+        return {"details" : "Invalid data"}, 400
+
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {"task": new_task.to_dict()}, 201
