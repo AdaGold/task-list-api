e059ff06205fee8c3d352d1bb1a94704079525b3
Ariam Yonas <ariam.yonas@hotmail.de>
2022-11-08 07:59:22 -0800

    set up model, added endpoint for all tasks

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,15 @@
-from flask import Blueprint
+from crypt import methods
+from flask import Blueprint,jsonify,make_response,request,abort
+from app.models.task import Task 
+from app import db
+
+tasks_bp = Blueprint('tasks_bp',__name__, url_prefix='/tasks')
+
+@tasks_bp.route('',methods = ['GET'])
+def get_all_tasks():
+    all_tasks = Task.query.all()
+    result = []
+
+    for task in all_tasks:
+        result.append(task.to_dict())
+    return jsonify(result), 200
