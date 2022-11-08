dc0cd912625e30af1c00f8c9ae72c1406a69588a
Aisha Husseini <ayasha.husseini@gmail.com>
2022-11-08 15:56:28 -0500

    pushing wave 6

--- app/__init__.py
+++ app/__init__.py
@@ -22,12 +22,14 @@ def create_app(test_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
-    # Import models here for Alembic setup
-    from app.models.task import Task
-    from app.models.goal import Goal
+# Import models here for Alembic setup
+    # from app.models.task import Task
+    # from app.models.goal import Goal
+
 
     db.init_app(app)
     migrate.init_app(app, db)
+
 
     # Register Blueprints here
 
@@ -36,8 +38,5 @@ def create_app(test_config=None):
 
     from .routes import goals_bp
     app.register_blueprint(goals_bp)
-
-    from app.models.task import Task
-    from app.models.goal import Goal
 
     return app
