2a742245ad13929f4f0df20295f88fad5a301707
Maggie <mjtice0@gmail.com>
2022-11-08 09:45:32 -0700

    Refactored goal_routes.py to use validate_model helper function and removed the validate_goal function within goal_routes.py

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,27 +1,32 @@
 from app import db
 from app.models.goal import Goal
 from flask import Blueprint, request, make_response, jsonify, abort
+
 from app.routes.route_helpers import validate_model
+
 
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
 #Get all goals - GET 
 @goals_bp.route("", methods = ["GET"])
 def get_goals():
+
+#get all of the goals 
     goals = Goal.query.all()
     goals_response = [] 
 
+#loop through all goals, append to goals_response list, call helper function to create dict
     for goal in goals:
         goals_response.append(goal.to_dict())
 
     return jsonify(goals_response)
 
-
-#CReate a goal - POST
+#Create a new goal - POST
+#Refactor ? 
 @goals_bp.route("", methods=['POST'])
 def create_goal():
+
     try:
-       
         request_body = request.get_json()
         new_goal = Goal(title = request_body["title"])
 
@@ -32,40 +37,25 @@ def create_goal():
         "goal": new_goal.to_dict()
    }, 201
 
-    except KeyError:
-        
+    except KeyError: 
         abort(make_response({"details": "Invalid data"}, 400))
-
-
-#validate goals
-def validate_goal(goal_id):
-    try:
-        goal_id = int(goal_id)
-    except: 
-        abort(make_response({"message": f"{goal_id} is invalid"}, 400))
-
-    goal = Goal.query.get(goal_id)
-
-    if not goal:
-        abort(make_response({"message": f"Goal {goal_id} not found"}, 404))
-
-    return goal
-
 
 #Get a goal - GET 
 @goals_bp.route("/<goal_id>", methods=['GET'])
 def get_one_goal(goal_id):
+
     #call helper function to validate the task_id
-    goal = validate_goal(goal_id)
+    goal = validate_model(Goal, goal_id)
     
-    # return dictionary with goal data
+    #return dictionary with goal data, call helper function
     return {"goal": goal.to_dict()}
 
-#Update a goal - put
+#Update a goal - PUT
 @goals_bp.route("/<goal_id>", methods=['PUT'])
 def update_goal(goal_id):
+
     #validate goal using helper function
-    goal = validate_goal(goal_id)
+    goal = validate_model(Goal, goal_id)
 
     request_body = request.get_json()
 
@@ -73,17 +63,33 @@ def update_goal(goal_id):
 
     db.session.add(goal)
     db.session.commit()
-    
-    # return dictionary with goal data
+
+    #return dictionary with goal data, call helper function
     return {"goal": goal.to_dict()}
 
 #Delete a goal - delete
 @goals_bp.route("/<goal_id>", methods=["DELETE"])
 def delete_goal(goal_id):
+
    #validate goal using helper function
-   goal = validate_goal(goal_id)
+   goal = validate_model(Goal, goal_id)
 
    db.session.delete(goal)
    db.session.commit()
    
    return make_response({"details": f"Goal {goal.goal_id} \"{goal.title}\" successfully deleted"})
+
+# #validate goals
+# #update to use validate models function
+# def validate_goal(goal_id):
+#     try:
+#         goal_id = int(goal_id)
+#     except: 
+#         abort(make_response({"message": f"{goal_id} is invalid"}, 400))
+
+#     goal = Goal.query.get(goal_id)
+
+#     if not goal:
+#         abort(make_response({"message": f"Goal {goal_id} not found"}, 404))
+
+#     return goal
