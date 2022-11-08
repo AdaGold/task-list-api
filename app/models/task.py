9eadae05eb765a6cdeaa19e31b5c46b3b2051b7e
Jennifer <datateur@gmail.com>
2022-11-07 20:44:58 -0800

    Created GET route for one task and validate task id helper function

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,18 @@ from app import db
 
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime, nullable=True, default=None)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def from_dict(self):
+        pass
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": False
+        }
--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,40 @@ from app import db
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message":f"Task #{task_id} invalid"}, 400))
+
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message":f"Task #{task_id} not found"}, 404))
+
+    return task
+
 @tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
     tasks = Task.query.all()
-    tasks_response = []
-
-    for task in tasks:
-        tasks_response.append({'id':task['id'],
-        'name':task['name'],
-        'description': task['description'],
-        'completed_at': task['completed_at']})
+    tasks_response = [task.to_dict() for task in tasks]
     
     return jsonify(tasks_response), 200
 
+@tasks_bp.route("", methods=["POST"])
+def create_one_task():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"],
+                    is_complete=request_body["completed_at"])
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"Task {new_task.title} successfully created", 201)
+
+@tasks_bp.route("/<id>", methods=["GET"])
+def get_one_task(id):
+    task = validate_task(id)
+
+    return jsonify({"task":task.to_dict()})
