d4a55c3edfc17f4aac03655fcb480a4c82fca012
Jennifer <datateur@gmail.com>
2022-11-07 18:11:20 -0800

    Created GET route to get all tasks

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, make_response, abort
+from app.models.task import Task
+from app import db
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+
+    for task in tasks:
+        tasks_response.append({'id':task['id'],
+        'name':task['name'],
+        'description': task['description'],
+        'completed_at': task['completed_at']})
+    
+    return jsonify(tasks_response), 200
+
