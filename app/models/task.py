5142d45cf89186e5035a206bbadee3cdc15373a2
dainiz <dainiz.almazan@gmail.com>
2022-11-08 19:57:33 -0800

    Adds Delete endpoint, validate_request() method, and fixes bugs in Task model

--- app/models/task.py
+++ app/models/task.py
@@ -2,23 +2,23 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(length=40), nullable=False)
     description = db.Column(db.String, nullable=False)
-    completed_at = db.Column(db.DateTime, nullable=True)
+    completed_at = db.Column(db.DateTime, default=None)
     is_complete = db.Column(db.Boolean, default=False)
 
     @classmethod
     def from_dict(cls, task_data):
         return Task(
-            title = task_data["title"],
-            description = task_data["description"],
-            completed_at = task_data["completed_at"]
+            title = task_data.get("title"),
+            description = task_data.get("description"),
+            completed_at = task_data.get("completed_at", None)
         )
-    
+
     def to_dict(self):
         return dict(
-            task_id = self.task_id,
+            id = self.id,
             title = self.title,
             description = self.description,
             is_complete = self.is_complete
--- app/routes/task.py
+++ app/routes/task.py
@@ -16,10 +16,18 @@ def validate_model(cls, model_id):
     
     return model
 
+def validate_request(request_body):
+    try:
+        request_body["title"]
+        request_body["description"]
+    except:
+        abort(make_response({"details": "Invalid data"}, 400))
+
 # CREATE
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
+    validate_request(request_body)
     new_task = Task.from_dict(request_body)
 
     db.session.add(new_task)
@@ -40,7 +48,7 @@ def handle_tasks():
 def handle_one_task(task_id):
     task = validate_model(Task, task_id)
     
-    response_body = {"task": task}
+    response_body = {"task": task.to_dict()}
 
     return jsonify(response_body), 200
 
@@ -61,3 +69,12 @@ def update_task(task_id):
     return make_response(jsonify(response_body)), 200
 
 # DELETE
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+    response_body = f"Task {task_id} \"{task.title}\" successfully deleted"
+
+    db.session.delete(task)
+    db.session.commit()
+    
+    return make_response({"details": response_body}, 200)
