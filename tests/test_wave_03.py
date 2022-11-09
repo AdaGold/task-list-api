2bc93efa2511ff51581294d202b7fa3fdb0ed992
Nicole Mejia <snicolemejia@gmail.com>
2022-11-09 15:26:00 -0700

    all the things

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from app.models.task import Task
 import pytest
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_on_incomplete_task(client, one_task):
     # Arrange
     """
@@ -59,7 +59,7 @@ def test_mark_incomplete_on_complete_task(client, completed_task):
     }
     assert Task.query.get(1).completed_at == None
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_on_completed_task(client, completed_task):
     # Arrange
     """
