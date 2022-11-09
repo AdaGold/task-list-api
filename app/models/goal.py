ff41d090bdfcdcad6dfc7075821d78e54ccda77f
dainiz <dainiz.almazan@gmail.com>
2022-11-09 11:34:17 -0800

    Establishes one-to-many relationship between goal and tasks

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,6 +4,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(length=40), nullable=False)
+    tasks = db.relationship("Task", back_populates="goal")
 
     @classmethod
     def from_dict(cls, task_data):
