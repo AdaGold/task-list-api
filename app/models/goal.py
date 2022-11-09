b93002b53d45b60e15e9fc8b33548f2afa7fe852
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-11-09 06:59:14 -0800

    Updated Goal model and implemented to_dict instance method and from_dict class method

--- app/models/goal.py
+++ app/models/goal.py
@@ -2,4 +2,19 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+
+    def to_dict(self):
+        result = {
+            "id": self.task_id,
+            "title": self.title
+            }
+
+        return result
+    
+    @classmethod
+    def from_dict(cls, task_dict):
+        return cls(
+            title=task_dict["title"]
+        )
