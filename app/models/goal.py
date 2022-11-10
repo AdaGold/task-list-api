78c83c7ce06a929225c44d37d32011b711ee8e3a
samiya <samiyai96@gmail.com>
2022-11-10 12:30:24 -0500

    added procfile

--- app/models/goal.py
+++ app/models/goal.py
@@ -17,12 +17,3 @@ class Goal(db.Model):
         return goal_as_dict
 
 
-#class Goal(db.Model):
-    #goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    #title = db.Column(db.String)
-    #tasks = db.relationship("Task", back_populates="goal")
-    #def to_dict(self):
-        #return {
-       # "id": self.goal_id,
-       # "title":self.title
-       # }
