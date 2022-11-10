b06948a8d6e07c86ba00b8cd1b450a915cc59d37
Maggie <mjtice0@gmail.com>
2022-11-09 21:30:48 -0700

    migrations

--- migrations/versions/4f02f55398a8_migration_changes.py
+++ migrations/versions/4f02f55398a8_migration_changes.py
@@ -1,8 +1,8 @@
-"""restart
+"""migration changes
 
-Revision ID: 2a0c3b6ce408
+Revision ID: 4f02f55398a8
 Revises: 
-Create Date: 2022-11-09 15:08:39.124408
+Create Date: 2022-11-09 21:29:45.937430
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2a0c3b6ce408'
+revision = '4f02f55398a8'
 down_revision = None
 branch_labels = None
 depends_on = None
