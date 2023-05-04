ace29846cddc637938d2773ce1c421de497516f8
Trang Tran <trantrang181197@gmail.com>
2023-05-04 12:25:31 -0700

    creat read, create, update, and delete routes with helper functions from Task model

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,64 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request, abort
+from app import db
+from app.models.task import Task
+
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+
+def validate_task(request_body):
+    try:
+        new_task = Task.from_dict(request_body)
+    except KeyError:
+        return make_response({"details": "Invalid data"}, 400)
+    return new_task
+
+@task_bp.route("", methods=["POST"])
+def create_one_task():
+    request_body = request.get_json()
+    new_task = validate_task(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response({"task": new_task.to_dict()}, 201)
+
+@task_bp.route("", methods=["GET"])
+def get_all_tasks():
+    response = []
+    tasks = Task.query.all()
+    
+    for task in tasks:
+        response.append(task.to_dict())
+
+    return jsonify(response), 200
+
+def validate_id(cls, id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message": f"{cls.__name__} {id} invalid"}, 400))
+    record = cls.query.get_or_404(id)
+    return record
+
+@task_bp.route("/<task_id>", methods=["GET"])
+def get_one_task(task_id):
+    task = validate_id(Task, task_id)
+    return make_response({"task": task.to_dict()}, 200)
+
+@task_bp.route("/<task_id>", methods=["PUT"])
+def update_one_task(task_id):
+    task = validate_id(Task, task_id)
+    request_body = request.get_json()
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+    return make_response({"task": task.to_dict()}, 200)
+
+@task_bp.route("/<task_id>", methods=["DELETE"])
+def delete_one_task(task_id):
+    task = validate_id(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return make_response({"details": f"Task {task.id} \"{task.description}\" successfully deleted"}, 200)
