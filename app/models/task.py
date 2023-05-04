7494d44e530aee584f9cc169ffa52cf3254d0d67
Trang Tran <trantrang181197@gmail.com>
2023-05-04 12:24:14 -0700

    create Task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,29 @@
 from app import db
 
-
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    
+    def to_dict(self):
+        if not self.completed_at:
+            is_complete = False
+        else:
+            is_complete = True
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": is_complete
+        }
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = cls(
+            title = task_data["title"],
+            description = task_data["description"],
+            completed_at = task_data["completed_at"]
+        )
+        return new_task
+
