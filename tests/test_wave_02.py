adb12d2cb59b91ca5a36999dae8ad57158854aef
Trang Tran <trantrang181197@gmail.com>
2023-05-06 15:17:21 -0700

    updated test_wave_02 to fit changes in Task model

--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -13,16 +13,19 @@ def test_get_tasks_sorted_asc(client, three_tasks):
     assert response_body == [
         {
             "id": 2,
+            "goal_id": None,
             "title": "Answer forgotten email 📧",
             "description": "",
             "is_complete": False},
         {
             "id": 3,
+            "goal_id": None,
             "title": "Pay my outstanding tickets 😭",
             "description": "",
             "is_complete": False},
         {
             "id": 1,
+            "goal_id": None,
             "title": "Water the garden 🌷",
             "description": "",
             "is_complete": False}
@@ -42,16 +45,19 @@ def test_get_tasks_sorted_desc(client, three_tasks):
         {
             "description": "",
             "id": 1,
+            "goal_id": None,
             "is_complete": False,
             "title": "Water the garden 🌷"},
         {
             "description": "",
             "id": 3,
+            "goal_id": None,
             "is_complete": False,
             "title": "Pay my outstanding tickets 😭"},
         {
             "description": "",
             "id": 2,
+            "goal_id": None,
             "is_complete": False,
             "title": "Answer forgotten email 📧"},
     ]
