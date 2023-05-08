26ada4bbc74e64429e1e0a6f4908fa5572e945c2
Kat <kmunson13@gmail.com>
2023-05-07 22:03:03 -0400

    Add Task model, register blueprint

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,26 @@
 from app import db
+from datetime import datetime
 
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    def to_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.task_id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["is_complete"] = self.completed_at
+
+        return task_as_dict
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"],
+                    description=task_data["description"],
+                    completed_at=task_data["completed_at"])
+        return new_task
+
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, abort, request
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task.from_dict(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify({'task': new_task.to_dict()}), 201
