0ad36422b15d39b5a791b06494d930733acf049a
Allie Soliz <solizale@gmail.com>
2023-05-08 00:27:44 -0700

    passes delete_task tests

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -144,7 +144,7 @@ def test_update_task_not_found(client):
     }
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task(client, one_task):
     # Act
     response = client.delete("/tasks/1")
@@ -159,7 +159,7 @@ def test_delete_task(client, one_task):
     assert Task.query.get(1) == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_task_not_found(client):
     # Act
     response = client.delete("/tasks/1")
@@ -168,15 +168,17 @@ def test_delete_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
 
     assert Task.query.all() == []
-
+    assert response_body == {"details": f"task 1 not found"}
 
 # @pytest.mark.skip(reason="No way to test this feature yet")
+
+
 def test_create_task_must_contain_title(client):
     # Act
     response = client.post("/tasks", json={
