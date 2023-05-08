53f93cfd0b38c6d786bf35f6beed5ddba2749741
Allie Soliz <solizale@gmail.com>
2023-05-07 21:19:43 -0700

    instantiates tasks_bp Blueprint instance

--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,5 @@ from app import db
 from app.models.task import Task
 from flask import Blueprint, jsonify, abort, make_response, Request
 
+# Instantiate Blueprint instances here
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
