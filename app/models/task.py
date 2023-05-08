1ba656df86736e6572ea087c29acc828af94ba52
Allie Soliz <solizale@gmail.com>
2023-05-07 23:32:02 -0700

    refactors task model

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,9 @@ from app import db
 
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    
