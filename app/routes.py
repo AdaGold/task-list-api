875e8a728f5c2b3d3454cc9339e1590b17c7aa63
Allie Soliz <solizale@gmail.com>
2023-05-10 21:23:09 -0700

    imports requests and os

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,8 @@ from app import db
 from app.models.task import Task
 from flask import Blueprint, jsonify, abort, make_response, request
 from datetime import datetime
+import requests
+import os
 
 # Instantiate Blueprint instances here
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
