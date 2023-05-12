e92cfcc8b38f1503e446cb96786d109e1554b118
Allie Soliz <solizale@gmail.com>
2023-05-11 22:08:56 -0700

    updates goals_bp to import from .goal_routes file

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -8,3 +8,103 @@ import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+# Instantiate Blueprint instances here
+goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
+
+
+# *************************************************************************
+# ********************************* GOALS *********************************
+# *************************************************************************
+
+
+def validate_goal(goal_id):
+    try:
+        goal_id = int(goal_id)
+    except:
+        abort(make_response({"details": f"Invalid data"}, 400))
+
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        abort(make_response({"details": f"Goal {goal_id} not found"}, 404))
+
+    return goal
+
+
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+
+    if not "title" in request_body:
+        abort(make_response({"details": "Invalid data"}, 400))
+
+    new_goal = Goal(title=request_body["title"])
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    # return make_response(jsonify(f"Task {new_task.title} successfully created"), 201)
+    return make_response(jsonify({
+        "goal": {
+            "id": new_goal.goal_id,
+            "title": new_goal.title
+
+        }
+    }), 201)
+
+
+@goals_bp.route("", methods=["GET"])
+def get_all_goals():
+    goals = Goal.query.all()
+
+    goal_response = []
+    for goal in goals:
+        goal_response.append(
+            {
+                "id": goal.goal_id,
+                "title": goal.title,
+
+            }
+        )
+    return jsonify(goal_response)
+
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def get_one_goal(goal_id):
+    goal = validate_goal(goal_id)
+    return make_response(jsonify({
+        "goal": {
+            "id": goal.goal_id,
+            "title": goal.title,
+        }
+    }), 200)
+
+
+@goals_bp.route("/<goal_id>", methods=["PUT"])
+def update_goal(goal_id):
+    goal = validate_goal(goal_id)
+
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+
+    db.session.commit()
+
+    return make_response(jsonify({
+        "goal": {
+            "id": goal.goal_id,
+            "title": goal.title,
+        }
+    }), 200)
+
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_goal(goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response(
+        {"details": f"Goal {goal_id} \"{goal.title}\" successfully deleted"}, 200)
