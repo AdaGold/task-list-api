f38370f132ffe49dc22425c29051c5180a5cfa1a
Allie Soliz <solizale@gmail.com>
2023-05-12 23:29:57 -0700

    adds help function to_dict()

--- app/models/task.py
+++ app/models/task.py
@@ -11,11 +11,20 @@ class Task(db.Model):
     goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
-        task_as_dict = {}
-        task_as_dict["id"] = self.task_id
-        task_as_dict["title"] = self.title
-        task_as_dict["description"] = self.description
-        task_as_dict["completed_at"] = self.completed_at
+        if self.completed_at:
+            completed = True
+        else:
+            completed = False
+
+        task_as_dict = {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": completed,
+        }
+
+        if self.goal_id:
+            task_as_dict["goal_id"] = self.goal_id
 
         return task_as_dict
 
