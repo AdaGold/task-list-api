2323fad99fd86e8704125ad3b8858295c6ad27f4
Charday Neal <charday.neal@gmail.com>
2024-11-01 13:44:19 -0700

    Create Task Model, Initial Setup, Enable Tests
    Complete initial setup of project, including migrations directory being
    using for Task Model changes.
    
    Create tasks endpoint for creating, reading, updating and deleting.
    
    Enable few tests from Wave 01

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,20 @@
+from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]]
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": True if self.completed_at else False
+        }
+        
+    
