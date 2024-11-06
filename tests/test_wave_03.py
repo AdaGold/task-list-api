5d12513ff2ed268d7703e8e040c5bf16fcc9f315
Charday Neal <charday.neal@gmail.com>
2024-11-06 13:39:54 -0800

    Test exceptions are being raised
    Implemented some asserts to a few tests to ensure our api responds
    appropriately to an invalid request and that exception are in fact
    raised.

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -4,22 +4,9 @@ from datetime import datetime
 from app.models.task import Task
 import pytest
 
-
 # @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_on_incomplete_task(client, one_task):
     # Arrange
-    """
-    The future Wave 4 adds special functionality to this route,
-    so for this test, we need to set-up "mocking."
-
-    Mocking will help our tests work in isolation, which is a
-    good thing!
-
-    We need to mock any POST requests that may occur during this
-    test (due to Wave 4).
-
-    There is no action needed here, the tests should work as-is.
-    """
     with patch("requests.post") as mock_get:
         mock_get.return_value.status_code = 200
 
@@ -65,18 +52,6 @@ def test_mark_incomplete_on_complete_task(client, completed_task):
 # @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_on_completed_task(client, completed_task):
     # Arrange
-    """
-    The future Wave 4 adds special functionality to this route,
-    so for this test, we need to set-up "mocking."
-
-    Mocking will help our tests work in isolation, which is a
-    good thing!
-
-    We need to mock any POST requests that may occur during this
-    test (due to Wave 4).
-
-    There is no action needed here, the tests should work as-is.
-    """
     with patch("requests.post") as mock_get:
         mock_get.return_value.status_code = 200
 
@@ -119,7 +94,7 @@ def test_mark_incomplete_on_incomplete_task(client, one_task):
     assert Task.query.get(1).completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_complete_missing_task(client):
     # Act
     response = client.patch("/tasks/1/mark_complete")
@@ -127,14 +102,11 @@ def test_mark_complete_missing_task(client):
 
     # Assert
     assert response.status_code == 404
+    assert not "task" in response_body
+    assert response_body == {"message": f"Invalid request: Task 1 not found"}
+    
 
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
-
-
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_mark_incomplete_missing_task(client):
     # Act
     response = client.patch("/tasks/1/mark_incomplete")
@@ -142,8 +114,5 @@ def test_mark_incomplete_missing_task(client):
 
     # Assert
     assert response.status_code == 404
-
-    raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
+    assert not "task" in response_body
+    assert response_body == {"message": f"Invalid request: Task 1 not found"}
